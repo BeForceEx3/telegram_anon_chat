6a0221e9d226e8092d28139dac0551c97fa330c1
BeForceEx3 <142899996+BeForceEx3@users.noreply.github.com>
2025-12-05 22:44:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = '7948397754:AAEl-qnWbzuyzYs3HmBIVCWlVzRHxAoG8-g'  # От @BotFather
-ADMIN_ID = 7911138642          # Ваш ID из @userinfobot
-DATA_PATH = 'data/chat_data.json'
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMIN_ID = int(os.getenv('ADMIN_ID', 0))
+DATA_PATH = '/tmp/chat_data.json'  # Render tmp/
